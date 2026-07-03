3f247e115e7c02925f86ad91e449b3402cda04e1
Ameer Zaman <150048864+Ameer-Zaman@users.noreply.github.com>
2026-07-03 16:15:45 +0530

    Add files via upload

---
+++ Login Beginner/app.js
@@ -0,0 +1,10 @@
+function login(){
+    let userName = document.getElementById("username").value;
+    let password = Number(document.getElementById("password").value);
+    
+    if(userName === "Ameer" && password === 1234){
+        window.location.href = "home.html";
+    } else {
+        window.alert("Invalid Password!");
+    }
+}
